7a3a4c440d5a15f46ff1f6c9a647865b25460c50
tommym0ntana <tommy.montana.harwood@gmail.com>
2020-01-23 21:38:26 +0100

    [setup and sign in] - update the signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,20 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+      flash[:success] = 'User created'
+    else
+      render 'new'
+    end
   end
 
   def show
+    @user = User.find(params[:id])
   end
+
 end
