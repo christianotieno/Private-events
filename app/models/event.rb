cae3055968ead80ff64f9e4640d7ad56be067258
ChristianOtieno <cgao273@gmail.com>
2020-01-25 01:06:32 +0300

    [Basic Events] - Add the association between the event creator (a User) and the event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,10 @@
 class Event < ApplicationRecord
-  belongs_to :creater, class_name: 'User',
-                       foreign_key: 'creator_id'
-  
-  has_many :avent_attendees, class_name: 'Attendance',
-                            foreign_key: 'attended_event_id'
+  # validates :title, presence: true
+  # validates :description, presence: true
+  # validates :location, presence: true
+  # validates :happening, presence: true
 
-  has_many :attendees, through: :attendancies, source: :attendee
-
+  belongs_to :creator, class_name: 'User' # references the creator_id in the User table
+  has_many :event_attendings, foreign_key: 'attended_event_id', class_name: 'Attendance'
+  has_many :attendees, through: :event_attendings, source: :attendee
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,7 @@
 class User < ApplicationRecord
-
-  has_many :attendees,   class_name: 'Attendance',
-                         foreign_key: 'attendee_id',
-                         dependent: :destroy
-
-  has_many :attended_events, through: :attendees, source: :attended_event
-                         
-  
+  has_many :created_events, class_name: 'Event', foreign_key: 'creator_id', dependent: :destroy
+  has_many :event_attendings, class_name: 'Attendance', foreign_key: 'attendee_id', dependent: :destroy
+  has_many :attended_events, through: :event_attendings, source: :attended_event, dependent: :destroy
 
   validates :name, presence: true
   validates :email, presence: true
