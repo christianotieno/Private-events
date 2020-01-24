e1fdd049c65ab52c98ab95ccead63affdada0996
tommym0ntana <tommy.montana.harwood@gmail.com>
2020-01-24 20:16:33 +0100

    [setup and sign in] - add associations between usern and event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,10 @@
 class Event < ApplicationRecord
+  belongs_to :creater, class_name: 'User',
+                       foreign_key: 'creator_id'
+  
+  has_many :avent_attendees, class_name: 'Attendance',
+                            foreign_key: 'attended_event_id'
+
+  has_many :attendees, through: :attendancies, source: :attendee
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
+
+  has_many :attendees,   class_name: 'Attendance',
+                         foreign_key: 'attendee_id',
+                         dependent: :destroy
+
+  has_many :attended_events, through: :attendees, source: :attended_event
+                         
+  
+
   validates :name, presence: true
   validates :email, presence: true
 end
