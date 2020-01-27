608fdef36d809e7f0fd1b7f3428b5b49ade4f574
ChristianOtieno <cgao273@gmail.com>
2020-01-27 22:58:28 +0300

    [Basic Events] - Add create new and show event functions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,21 @@
 class EventsController < ApplicationController
   def index; end
 
-  def new; end
+  def new
+    @event = current_user.created_events.build
+  end
 
-  def show; end
+  def show
+    @event = Event.find_by(id: params[:id])
+  end
 
-  def create; end
+  def create
+    @event = current_user.created_events.build(event_params)
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:tile, :description, :location, :happening)
+  end
 end
