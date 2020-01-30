9de535320678a94faef0ab2e7e53df837530d3d6
ChristianOtieno <cgao273@gmail.com>
2020-01-30 15:53:25 +0300

    [CODE REVIEW] - Rectify session login and signup of user problem

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,12 +1,12 @@
 module SessionsHelper
   # Logs in the given user.
-  def log_in(_user)
-    session[:user_id] = @user.id
+  def log_in(user)
+    session[:user_id] = user.id
   end
 
   # Returns the current logged-in user (if any).
   def current_user
-    @current_user ||= User.find_by(id: session[:id]) if session[:id]
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
   # Returns true if the user is logged in, false otherwise.
